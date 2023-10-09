a441be2902532d40bade37b5a7d16e5d869ab4dd
saurav <sauravlokhande24@gmail.com>
2023-10-10 01:35:47 +0530

    screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Signup from "./screens/Signup";
+import Login from "./screens/Login";
+import Resto from "./screens/Resto";
 
 function App() {
   return (
     <>
      <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Navbar  />}/>
+       
+      <Route path="/" element={<Navbar />}/>
+      <Route exact path="/signup" element={<Signup/>}/>
+      <Route exact path="/login" element={ <Login/>}/>
+      <Route exact path="/resto" element={<Resto/>}/>
       </Routes>
     </BrowserRouter>
     </>
