bebd6205cc1ff629b5c98aa8c37530e07e5fdbff
Vivek Mittal <mittal0623@gmail.com>
2023-10-09 23:36:58 +0530

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
-    <div className="App">
-     hello moto hello
-    </div>
+    <>
+     <Navbar/>
+    </>
   );
 }
 
