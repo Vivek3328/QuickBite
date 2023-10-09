a14045f0b55d6eaa2bad59e53d485af96e0adb47
Vivek Mittal <mittal0623@gmail.com>
2023-10-10 00:56:07 +0530

    add route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <>
-     <Navbar/>
+     <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Navbar  />}/>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
