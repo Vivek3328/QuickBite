c52fb8452793f85aeccea9a538c863c1b29e97f7
saurav <sauravlokhande24@gmail.com>
2023-10-11 01:17:53 +0530

    login & signup Popup

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -12,7 +12,7 @@ export default function Login() {
     e.preventDefault();
     
   }
-
+  
   return (
     <div className="container">
 
@@ -21,7 +21,7 @@ export default function Login() {
        
         <form onSubmit={handleSubmit}>
           <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address :</label>
+            {/* <label htmlFor="exampleInputEmail1">Email address :</label> */}
             <input
             type="text" placeholder="Email"
             value={email}     
@@ -31,7 +31,7 @@ export default function Login() {
           />            
           </div>
           <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password :</label>
+            {/* <label htmlFor="exampleInputPassword1">Password :</label> */}
             <input
             type="password" placeholder="Password"
             autoComplete="current-password"
@@ -40,7 +40,7 @@ export default function Login() {
           />
           </div>
           
-          <button type="submit" className="btn btn-dark">Submit</button>
+          <button type="submit"  className="btn btn-dark">Submit</button>
         </form>
         {/* <button className="btn waves-effect waves-light btn #d32f2f red darken-1">Login</button> */}
         <h6>
