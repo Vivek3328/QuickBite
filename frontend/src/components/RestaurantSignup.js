05eb923721be50ff27458a42c82daa1c3ea9471f
Vivek Mittal <mittal0623@gmail.com>
2023-10-11 23:50:23 +0530

    Restaurant signup update

--- frontend/src/components/RestaurantSignup.js
+++ frontend/src/components/RestaurantSignup.js
@@ -10,85 +10,135 @@ import {
     ModalCloseButton,
     Button,
     useDisclosure,
+    Radio, RadioGroup, Stack
 
 } from '@chakra-ui/react'
-
 import styles from "./styles/resto.module.css"
 
-export default function RestaurantSignup({authType}) {
+
+export default function RestaurantSignup({ authType }) {
     const navigate = useNavigate()
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [fullName, setFullName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-    }
-    if(authType==="signup"){
-        if(!isOpen){
+    if (authType === "signup") {
+        if (!isOpen) {
             onOpen();
         }
     }
-    else{
-        if(isOpen){
+    else {
+        if (isOpen) {
             onClose()
         }
     }
+
+    const [value, setValue] = React.useState('1')
+    const [formData, setFormData] = useState({
+        name: '',
+        address: '',
+        pincode: '',
+        phoneNumber: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission here, e.g., send the data to an API or perform any necessary actions.
+        console.log('Form submitted with data:', formData);
+    };
     return (
         <div className={styles.signupbtn}>
             <Button ><Link to="/Resto?authType=signup">Register your restaurant</Link></Button>
 
-            <Modal  isOpen={isOpen} onClose={()=>{
+            <Modal isOpen={isOpen} onClose={() => {
                 onClose();
                 navigate("/Resto")
             }}>
                 <ModalOverlay />
                 <ModalContent>
                     <ModalHeader>SignUp</ModalHeader>
-                    <ModalCloseButton  />
+                    <ModalCloseButton />
                     <ModalBody>
                         <form onSubmit={handleSubmit}>
-                            <div className="form-group">
+                            <div>
+                                <label htmlFor="name">Restaurant Name:</label>
                                 <input
-                                    type="text" placeholder="Name"
-                                    value={fullName}
-                                    autoFocus
-                                    autoComplete="username"
-                                    onChange={(event) => setFullName(event.target.value)}
-
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                    required
                                 />
                             </div>
-                            <div className="form-group">
-                                <input
-                                    type="text" placeholder="Email"
-                                    value={email}
-                                    autoComplete="username"
-                                    onChange={(event) => setEmail(event.target.value)}
-
+                            <div>
+                                <label htmlFor="address">Complete Address:</label>
+                                <textarea
+                                    id="address"
+                                    name="address"
+                                    value={formData.address}
+                                    onChange={handleChange}
+                                    required
                                 />
                             </div>
-                            <div className="form-group">
+                            <div>
+                                <label htmlFor="pincode">Pincode:</label>
                                 <input
-                                    type="password" placeholder="Password"
-                                    autoComplete="current-password"
-                                    value={password}
-                                    onChange={(event) => setPassword(event.target.value)}
+                                    type="text"
+                                    id="pincode"
+                                    name="pincode"
+                                    value={formData.pincode}
+                                    onChange={handleChange}
+                                    required
                                 />
                             </div>
+                            <div>
+                                <label htmlFor="phoneNumber">Phone Number:</label>
+                                <input
+                                    type="text"
+                                    id="phoneNumber"
+                                    name="phoneNumber"
+                                    value={formData.phoneNumber}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <RadioGroup onChange={setValue} value={value}>
+                                <Stack direction='row'>
+                                    <label htmlFor="">Restaurant type:</label>
+                                    <Radio value='veg'>Veg</Radio>
+                                    <Radio value='non-veg'>Non-veg</Radio>
+                                </Stack>
+                            </RadioGroup>
+                            <div className="mb-1">
+                                Image <span className="font-css top">*</span>
+                                <div className="">
+                                    <input type="file" id="file-input" name="ImageStyle" accept="image/*" />
+                                </div>
+                            </div>
 
-                           
+                            <ModalFooter style={{ justifyContent: 'center', paddingBottom: '0' }} >
+
+                                <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                    SignUp
+                                </Button>
+
+                                {/* <Button variant='ghost'>Secondary Action</Button> */}
+                            </ModalFooter>
+
+
                         </form>
                     </ModalBody>
 
-                    <ModalFooter style={{ justifyContent: 'center', paddingBottom: '0' }} >
 
-                        <Button colorScheme='blue' mr={3} onClick={onClose}>
-                            SignUp
-                        </Button>
-
-                        {/* <Button variant='ghost'>Secondary Action</Button> */}
-                    </ModalFooter>
                     <div className={styles.revert}>
                         <h6>
                             <Link to="/Resto?authType=login">Already have an account ?</Link>
