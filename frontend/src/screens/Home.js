6684e86e387feb530ac780100d163b8a2beb89e2
Vivek Mittal <94603870+Vivek3328@users.noreply.github.com>
2023-10-12 16:53:54 +0530

    Revert "user & menu page"

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import styles from "../screens/styles/home.module.css"
-import { Link,useSearchParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Login from './Login'
 import Signup from './Signup'
-
-
 // import RestaurantLogin from '../components/RestaurantLogin'
 // import RestaurantSignup from '../components/RestaurantSignup'
 // import Resto from './Resto'
-export default function Home() {
-    const [params] = useSearchParams();
-    let userAuthType=params.get("userAuthType")
+export default function home() {
+
     return (
         <>
             <div className={styles.btns}>
-               
-                <Login userAuthType={userAuthType}></Login>
-                <Signup userAuthType={userAuthType}></Signup>
-                
+                {/* <button className={styles.btn}>Login</button>
+            <button className={styles.btn}>Sign UP</button> */}
+                <Login/>
+                <Signup/>
                 <div className={styles.addRestoBtn} ><Link className={styles.btn} to="/Resto">Add Restaurant</Link></div>
             </div>
             <div className={styles.homeScreen} >
