f2a86de891204913b7f09cf255bd63d5b2bb2f63
saurav <sauravlokhande24@gmail.com>
2023-10-23 02:00:39 +0530

    cart

--- frontend/src/screens/MenuCard.js
+++ frontend/src/screens/MenuCard.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Card, CardBody, Stack, Heading, Text, Image, Button, CardFooter, Select } from '@chakra-ui/react'
+import cartContext from '../Context/cartContext';
+import { useLocation } from 'react-router-dom';
+
 
 export default function MenuCard(props) {
+
+    const a = useContext(cartContext);
+    const location = useLocation();
+    const [amt, setAmt] = useState(0);
+
+    const handlePrice = () => {
+        let ans = 0;
+        //eslint-disable-next-line 
+        a.cart.map((it) => {
+            ans += it.cnt * it.price;
+        })
+        setAmt(ans);
+    }
+    const handleRemove =(id) =>{
+        const arr = a.cart.filter((item)=>item._id !== id);
+        a.setCart(arr);
+    }
+    useEffect(() => {
+        handlePrice();
+        //eslint-disable-next-line 
+    }, [])
+
+    // console.log(location);
     return (
         <div>
-         
+
             <Card
                 direction={{ base: 'column', sm: 'row' }}
                 overflow='hidden'
@@ -14,8 +40,8 @@ export default function MenuCard(props) {
             >
                 <Image
                     // objectFit='cover'
-                    maxW={{ base: '500%', sm: '300px' }}
-                    src=  {props.image}
+                    maxW={{ base: '100%', sm: '300px' }}
+                    src={props.image}
                     alt='Caffe Latte'
                     borderRadius='3xl'
                     p='2'
@@ -23,16 +49,16 @@ export default function MenuCard(props) {
 
                 <Stack>
                     <CardBody>
-                        <div style={{ display: 'flex'}}>
-                        <Heading size='md'>{props.itemname}
-                            <Image
-                              maxW={{ base: '100%', sm: '40px' }}
-                              src='https://img.icons8.com/?size=96&id=61083&format=png'
-                              alt='Veg'
-                              borderRadius='3xl'
-                              p='2'
-                             />
-                        </Heading>
+                        <div style={{ display: 'flex' }}>
+                            <Heading size='md'>{props.itemname}
+                                <Image
+                                    maxW={{ base: '100%', sm: '40px' }}
+                                    src='https://img.icons8.com/?size=96&id=61083&format=png'
+                                    alt='Veg'
+                                    borderRadius='3xl'
+                                    p='2'
+                                />
+                            </Heading>
                         </div>
                         <Heading py='1' size='sm'>
                             Price : {props.price} Rs.
@@ -43,22 +69,50 @@ export default function MenuCard(props) {
                     </CardBody>
 
                     <CardFooter>
-                        <div style={{ display: 'flex', justifyContent: 'center', marginRight: '27px' }}>
-                            <Select placeholder='Select Quantity'>
-                                <option value='1'> 1</option>
-                                <option value='2'> 2</option>
-                                <option value='3'> 3</option>
-                                <option value='4'> 4</option>
-                                <option value='5'> 5</option>
-                            </Select>
-                        </div>
-                        <Button variant='outline' colorScheme='green' pl='5' >
-                            Add to Cart
-                        </Button>
+                        {location.pathname === '/cart' ? (<>
+                            <div style={{ display: 'flex', justifyContent: 'end'}}>
+                                <div className='btns' >
+                                    <Button variant='solid' colorScheme='red' pl='5' >
+                                        -
+                                    </Button>
+                                    <Button variant='outline' colorScheme='green' pl='5' value="cnt">
+                                        count
+                                    </Button>
+                                    <Button variant='solid' colorScheme='messenger' pl='5'>
+                                        +
+                                    </Button>
+                                </div>
+                                <div style={{paddingLeft:'200px' }}>
+                                    <Button variant='solid' colorScheme='red' pl='5' onClick={() => handleRemove(props._id)}>
+                                        Remove
+                                    </Button>
+                                </div>
+                            </div>
+                        </>) : (
+                            <>
+                                <div style={{ display: 'flex', justifyContent: 'center', marginRight: '27px' }}>
+                                    <Select placeholder='Select Quantity'>
+                                        <option value='1'> 1</option>
+                                        <option value='2'> 2</option>
+                                        <option value='3'> 3</option>
+                                        <option value='4'> 4</option>
+                                        <option value='5'> 5</option>
+                                    </Select>
+                                </div>
+                                <Button variant='outline' colorScheme='green' pl='5' onClick={() => a.handleclick(props.item)}>
+                                    Add to Cart
+                                </Button>
+                            </>
+                        )}
 
                     </CardFooter>
                 </Stack>
             </Card>
+            <div>
+                <span>
+                    Your Total Amount is : {amt} Rs.
+                </span>
+            </div>
         </div>
     )
 }
--- frontend/src/screens/RestoPage.js
+++ frontend/src/screens/RestoPage.js
@@ -2,13 +2,14 @@ import React, {useState, useEffect } from 'react'
 import { useParams} from 'react-router-dom';
 import axios from 'axios'
 import MenuCard from './MenuCard';
-
+import Navbar from '../components/Navbar';
 
 export default function RestoPage() {
   const { id } = useParams();
-  console.log(id)
+  // console.log(id)
 
   const [menu, setMenu] = useState([]);
+
   const updateMenu = async () => {
 
     await axios.get(`http://localhost:5000/api/menuitemauth/fetchrestomenu/${id}`)
@@ -25,6 +26,7 @@ export default function RestoPage() {
 
   return (
     <div>
+      <Navbar/>
       <div className="container">
 
         <div className="row my-3 item-center" style={{ padding: '10px', justifyContent: 'center' }} >
@@ -32,7 +34,7 @@ export default function RestoPage() {
           { menu?.map((element, index) => {
 
             return <div className="col-md-8" key={index} style={{ padding: '10px' }}>
-              <MenuCard itemname={element?.itemname} price={element?.price} image={element?.image ? element.image : "https://img.icons8.com/?size=96&id=61083&format=png"} description={element?.description}
+              <MenuCard itemname={element?.itemname} price={element?.price} image={element?.image ? element.image : "https://img.icons8.com/?size=96&id=61083&format=png"} description={element?.description} _id = {element._id} item = {element}
               />
             </div>
 
