04119e0b36d460aa0269a47e27b7854984a3b5da
nitishp007 <patel.nitish47@gmail.com>
2023-10-27 00:58:15 +0530

    orderAuth

--- backend/routes/OrderAuth.js
+++ backend/routes/OrderAuth.js
@@ -1,27 +1,42 @@
 const express = require("express");
-const fetchuser = require("../Middlewares/fetchuser");
-const { body, validationResult } = require("express-validator");
-const OrderItem = require("../models/OrderModel");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const Order= require("../models/OrderModel");
+const fetchuser = require("../Middlewares/fetchuser");
 
-// Route-1 : POST Items using 'api/orderauth/placeorder'
-router.post("/placeorder", fetchuser, async (req, res) => {
-        try {
-            const { ownerId, quantity, totalprice , menuitem} = req.body;
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                  return res.status(400).json({ errors: errors.array() });
-            }
-            const item = new OrderItem({
-                ownerId, quantity, totalprice, menuitem, user: req.user.id
-            })
-            const saveditem = await item.save();
-            res.json(saveditem)
-        } catch (error) {
-            console.error(error.message);
-            res.status(500).send("Internal server error occured");
-        }
+router.post("/checkout",fetchuser,async (req, res) => {
+    try {
+      const Item = req.body.item.map((item) => ({
+        menuitem: item.menuitem, 
+        quantity: item.quantity,
+    }));
+
+        order = await Order.create({
+            user:req.user,
+            owner:req.body.owner,
+            item:Item,
+            shipping:req.body.shipping,
+            totalprice:req.body.totalprice,
+            dateOrdered:req.body.dateOrdered,
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ success:false, error: "Some Error occured" });
     }
+}
 );
+
+
+router.get('myorders/:ownerId', async (req, res) => {
+  try {
+      const ownerId = req.params.ownerId;
+      const orders = await Order.find({ owner: ownerId });
+      res.status(200).json(orders);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 
 module.exports = router;
