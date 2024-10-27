cd5cdf4bfa8fab7320ce5f6d195c0b6edebd1a83
Vivek Mittal <mittal0623@gmail.com>
2024-10-27 20:14:04 +0530

    changes

--- backend/Middlewares/fetchowner.js
+++ backend/Middlewares/fetchowner.js
@@ -1,18 +1,3 @@
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET_KEY;
+const fetchOwner = async (req, res, next) => { }
 
-const fetchOwner = (req, res, next) => {
-  //get the user from jwt token and Add Id to req object
-  const token = req.header("auth-token");
-  if (!token) {
-    res.status(401).send({ error: "Please authenticate using a valid token" });
-  }
-  try {
-    const data = jwt.verify(token, JWT_SECRET);
-    req.owner = data.owner;
-    next();
-  } catch (error) {
-    res.status(401).send({ error: "Please authenticate using a valid token" });
-  }
-};
-module.exports = { fetchOwner };
+module.exports = fetchOwner
--- backend/routes/MenuItemAuth.js
+++ backend/routes/MenuItemAuth.js
@@ -8,28 +8,28 @@ const {
   updateMenuItem,
   deleteMenuItem,
 } = require("../controllers/MenuItemController");
-const { fetchOwner } = require("../middlewares/fetchOwner")
+const fetchOwner = require("../middlewares/fetchOwner")
 
 
 
-// router.post(
-//   "/additem",
-//   fetchOwner,
-//   [
-//     body("itemname", "Enter a valid Item Name").isLength({ min: 3 }),
-//     body("description", "Description must be of minimum 5 character").isLength({
-//       min: 5,
-//     }),
-//   ],
-//   addItem
-// );
+router.post(
+  "/additem",
+  fetchOwner,
+  [
+    body("itemname", "Enter a valid Item Name").isLength({ min: 3 }),
+    body("description", "Description must be of minimum 5 character").isLength({
+      min: 5,
+    }),
+  ],
+  addItem
+);
 
-// router.get("/fetchallmenuitems", fetchOwner, allmenuitems);
+router.get("/fetchallmenuitems", fetchOwner, allmenuitems);
 
 router.get("/fetchrestomenu/:id", restaurantMenu);
 
-// router.put("/updatemenuitem/:id", fetchOwner, updateMenuItem);
+router.put("/updatemenuitem/:id", fetchOwner, updateMenuItem);
 
-// router.delete("/deletemenuitems/:id", fetchOwner, deleteMenuItem);
+router.delete("/deletemenuitems/:id", fetchOwner, deleteMenuItem);
 
 module.exports = router;
