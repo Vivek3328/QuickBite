c0cecb7c49766dcd3f0bb3bc3de1787585b466ea
Vivek Mittal <mittal0623@gmail.com>
2024-10-27 19:49:57 +0530

    changes

--- backend/routes/OrderAuth.js
+++ backend/routes/OrderAuth.js
@@ -10,24 +10,24 @@ const {
 const { fetchOwner } = require("../middlewares/fetchOwner");
 const { fetchUser } = require("../middlewares/fetchUser")
 
-router.post(
-  "/checkout",
-  fetchUser,
-  [
-    body("owner", "Owner is required").not().isEmpty(),
-    body("item", "Items are required").isArray({ min: 1 }),
-    body("totalprice", "Total price is required and must be a number").isFloat({
-      gt: 0,
-    }),
-    body("shipping", "Shipping details are required").not().isEmpty(),
-  ],
-  checkout
-);
+// router.post(
+//   "/checkout",
+//   fetchUser,
+//   [
+//     body("owner", "Owner is required").not().isEmpty(),
+//     body("item", "Items are required").isArray({ min: 1 }),
+//     body("totalprice", "Total price is required and must be a number").isFloat({
+//       gt: 0,
+//     }),
+//     body("shipping", "Shipping details are required").not().isEmpty(),
+//   ],
+//   checkout
+// );
 
-router.get("/myorders/", fetchOwner, restaurantOrders);
+// router.get("/myorders/", fetchOwner, restaurantOrders);
 
-router.get("/userorders", fetchUser, userOrders);
+// router.get("/userorders", fetchUser, userOrders);
 
-router.put("/updateorder/:id", fetchOwner, updateStatus);
+// router.put("/updateorder/:id", fetchOwner, updateStatus);
 
 module.exports = router;
