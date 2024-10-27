7a6cf0622a29096b25324f23238ab380856b6ed8
Vivek Mittal <mittal0623@gmail.com>
2024-10-27 14:17:16 +0530

    changes

--- backend/routes/MenuItemAuth.js
+++ backend/routes/MenuItemAuth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
-const fetchOwner = require("../middlewares/fetchOwner.js");
+// const fetchOwner = require("../middlewares/fetchOwner.js");
 const {
   allmenuitems,
   addItem,
