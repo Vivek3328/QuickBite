562f407c2e7365bf534604c9ec660cf73660a6e7
Vivek Mittal <mittal0623@gmail.com>
2024-10-27 21:27:15 +0530

    changes

--- backend/Middlewares/fetchowner.js
+++ backend/Middlewares/fetchowner.js
@@ -1,3 +1,18 @@
-const FetchOwner = async (req, res, next) => { }
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+const FetchOwner = async (req, res, next) => {//get the user from jwt token and Add Id to req object
+    const token = req.header("auth-token");
+    if (!token) {
+        res.status(401).send({ error: "Please authenticate using a valid token" });
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        req.owner = data.owner;
+        next();
+    } catch (error) {
+        res.status(401).send({ error: "Please authenticate using a valid token" });
+    }
+}
 
 module.exports = FetchOwner
